e21a47021acaebb036eede6cf2da11242bd66dfe
magnolia1234 <7676006-magnolia1234@users.noreply.gitlab.com>
2020-02-21 18:31:48 +0100

    Show available new release in popup (click icon)
    Example:
    Bypass Paywalls Clean v1.6.3.1
    Options | GitHub | New release v1.6.3.x

--- version.js
+++ version.js
@@ -1,3 +1,19 @@
 var manifestData = chrome.runtime.getManifest();
 var versionString = 'v' + manifestData.version;
 document.getElementById('version').innerText = versionString;
+
+const manifest_new = 'https://raw.githubusercontent.com/magnolia1234/bypass-paywalls-chrome-clean/master/manifest.json';
+fetch(manifest_new)
+	.then(response => { 
+		if (response.ok) {
+			response.json().then(json => {
+				var version_new = json['version'];
+				if (version_new > manifestData.version) {
+					versionString_new = 'New release v' + version_new;
+				} else {
+					versionString_new  = 'Latest release';
+				}
+				document.getElementById('version_new').innerText = versionString_new;
+			})
+		}
+	});
