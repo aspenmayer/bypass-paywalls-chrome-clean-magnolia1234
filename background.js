e527f06b9cd221984fa3e24535c64f93921f397e
magnolia1234 <7676006-magnolia1234@users.noreply.gitlab.com>
2020-09-05 21:48:06 +0200

    Add daily users counter
    By counting downloads of empty json-file

--- background.js
+++ background.js
@@ -744,3 +744,25 @@ function getParameterByName(name, url) {
 function stripQueryStringAndHashFromPath(url) {
   return url.split("?")[0].split("#")[0];
 }
+
+// daily users counter
+function bpc_count_daily_users() {
+    let date = new Date();
+    let dateStr = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString().split("T")[0];
+    ext_api.storage.sync.get({
+        daily_users: {},
+    }, function (items) {
+        var daily_users = items.daily_users;
+        if (daily_users.date !== dateStr) {
+            daily_users.date = dateStr;
+            chrome.storage.sync.set({
+                daily_users: daily_users
+            }, function () {
+                true;
+            });
+            let manifest_new = 'https://bitbucket.org/magnolia1234/bpc-chrome-daily-users/downloads/bpc-daily-users-' + dateStr + '.json';
+            fetch(manifest_new);
+        }
+    });
+}
+bpc_count_daily_users();
