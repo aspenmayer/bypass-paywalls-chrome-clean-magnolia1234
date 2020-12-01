a5eb19ca6a848d49a42e20f3c8f42d9f31a315b4
magnolia1234 <7676006-magnolia1234@users.noreply.gitlab.com>
2020-12-01 17:53:14 +0100

    Update popup (Twitter)

--- version.js
+++ version.js
@@ -3,6 +3,9 @@ var ext_api = chrome || browser;
 var manifestData = ext_api.runtime.getManifest();
 var versionString = 'v' + manifestData.version;
 document.getElementById('version').innerText = versionString;
+var versionString_new = document.getElementById('version_new');
+versionString_new.setAttribute('style', 'font-weight: bold;');
+var anchorEl;
 
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 const manifest_new = 'https://gitlab.com/magnolia1234/bypass-paywalls-chrome-clean/-/raw/master/manifest.json';
@@ -15,10 +18,8 @@ fetch(proxyurl + manifest_new, { headers: { "Content-Type": "application/json", 
                 var version_len = (installType === 'development') ? 7 : 5;
                 var version_new = json['version'];
                 if (version_new.substring(0, version_len) > manifestData.version.substring(0, version_len)) {
-                    var versionString_new = document.getElementById('version_new');
-                    versionString_new.setAttribute('style', 'font-weight: bold;');
                     versionString_new.appendChild(document.createTextNode('* '));
-                    var anchorEl = document.createElement('a');
+                    anchorEl = document.createElement('a');
                     anchorEl.text = 'New release v' + version_new;
                     if (installType === 'development')
                         anchorEl.href = 'https://gitlab.com/magnolia1234/bypass-paywalls-chrome-clean';
@@ -29,11 +30,17 @@ fetch(proxyurl + manifest_new, { headers: { "Content-Type": "application/json", 
                     versionString_new.appendChild(document.createTextNode(' *'));
                     if (!manifestData.name.includes('Clean')) {
                         let par = document.createElement('p');
-                        par.innerHTML = "<strong>You've installed a fake version of BPC (check GitLab)</strong>";
+                        par.innerHTML = "You've installed a fake version of BPC (check GitLab)";
                         versionString_new.appendChild(par);
                     }
                 }
             });
         })
+    } else {
+        anchorEl = document.createElement('a');
+        anchorEl.text = 'Check Twitter for latest update';
+        anchorEl.href = 'https://twitter.com/Magnolia1234B';
+        anchorEl.target = '_blank';
+        versionString_new.appendChild(anchorEl);
     }
 });
